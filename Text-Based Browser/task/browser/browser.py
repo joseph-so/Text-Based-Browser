9d1262ecfa3a07278238826820c94a696edfd968
Joseph <joseph-so@gmx.net>
2020-05-02 23:01:10 +1000

    Complete Task 4

--- Text-Based Browser/task/browser/browser.py
+++ Text-Based Browser/task/browser/browser.py
@@ -1,41 +1,7 @@
 import os
 import sys
 from collections import deque
-
-nytimes_com = '''
-This New Liquid Is Magnetic, and Mesmerizing
-
-Scientists have created “soft” magnets that can flow 
-and change shape, and that could be a boon to medicine 
-and robotics. (Source: New York Times)
-
-
-Most Wikipedia Profiles Are of Men. This Scientist Is Changing That.
-
-Jessica Wade has added nearly 700 Wikipedia biographies for
- important female and minority scientists in less than two 
- years.
-
-'''
-
-bloomberg_com = '''
-The Space Race: From Apollo 11 to Elon Musk
-
-It's 50 years since the world was gripped by historic images
- of Apollo 11, and Neil Armstrong -- the first man to walk 
- on the moon. It was the height of the Cold War, and the charts
- were filled with David Bowie's Space Oddity, and Creedence's 
- Bad Moon Rising. The world is a very different place than 
- it was 5 decades ago. But how has the space race changed since
- the summer of '69? (Source: Bloomberg)
-
-
-Twitter CEO Jack Dorsey Gives Talk at Apple Headquarters
-
-Twitter and Square Chief Executive Officer Jack Dorsey 
- addressed Apple Inc. employees at the iPhone maker’s headquarters
- Tuesday, a signal of the strong ties between the Silicon Valley giants.
-'''
+import requests
 
 # write your code here
 args = sys.argv
@@ -57,28 +23,18 @@ while command != "exit":
         previousURL = url
         url = command
     if "." in url:
-        content = ""
-        found = False
-        if url == "bloomberg.com":
-            print(bloomberg_com)
-            content = bloomberg_com
-            found = True
 
-        if url == "nytimes.com":
-            print(nytimes_com)
-            content = nytimes_com
-            found = True
+        r = requests.get(url if url.startswith(("http://", "https://")) else 'https://'+url)
 
-        if found:
-            filepath = url.split(".")
-            filepath = ".".join(filepath[:-1])
+        content = str(r.content)
 
-            savedFile[filepath] = url
+        filepath = url.split(".")
+        filepath = ".".join(filepath[:-1])
 
-            with open(os.path.join(directory, url), "w") as f:
-                f.write(content)
-        else:
-            print("Error")
+        savedFile[filepath] = url
+
+        with open(os.path.join(directory, url), "w") as f:
+            f.write(content)
 
         print()
     elif url != "exit":
